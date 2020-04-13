9c2abfab340e3645b4b66798e27c8116dbfa52da
Jay <jay@JaydeMacBook-Pro.local>
2020-04-13 15:02:14 +0800

    初始化

--- .history/src/main_20200413025013.js
+++ .history/src/main_20200413025013.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/assets/scss/reset.scss'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import '@/assets/font-awesome-4.7.0/css/font-awesome.css'
-Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 console.log(
   `当前所处开发环境:【${process.env.NODE_ENV}】| 路由为: 【${process.env.VUE_APP_URL}】`
--- .history/src/main_20200413145407.js
+++ .history/src/main_20200413145407.js
@@ -3,10 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/scss/reset.scss'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import '@/assets/font-awesome-4.7.0/css/font-awesome.css'
-Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 console.log(
   `当前所处开发环境:【${process.env.NODE_ENV}】| 路由为: 【${process.env.VUE_APP_URL}】`
